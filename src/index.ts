70726dca0b3505289932de15b405db2636029499
Michelle Tilley <michelle@michelletilley.net>
2018-11-23 22:16:33 -0800

    Restore order of game systems; async emit fixes issue

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,6 @@ db.set("Faerhan", "hunter2")
 
 const PORT = parseInt(process.env.PORT || "3333", 10)
 const world = new ecs.World()
-// If multiple systems subscribe to an event, the event handlers
-// are called in order of subscription. Thus, more specialized
-// input handlers should go last to prevent duplicate input handling
-// when transitioning an entity from one system to another via component changes.
 world.addSystem(new NetworkSystem(PORT))
-world.addSystem(new GameInputSystem())
 world.addSystem(new LoginSystem(db))
+world.addSystem(new GameInputSystem())
