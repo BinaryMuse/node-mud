888d3a0cc12e6e8f22977ccafae17aa566f3d309
Michelle Tilley <michelle@michelletilley.net>
2018-11-23 22:02:18 -0800

    Move LoginSystem to end of system list

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import { NetworkSystem } from "./network"
 import { GameInputSystem, LoginSystem } from "./game-loop"
 
 
+// Some fake accounts
 const db = new Map()
 db.set("Celidur", "password")
 db.set("Faerhan", "hunter2")
 
 const PORT = parseInt(process.env.PORT || "3333", 10)
 const world = new ecs.World()
+// If multiple systems subscribe to an event, the event handlers
+// are called in order of subscription. Thus, more specialized
+// input handlers should go last to prevent duplicate input handling
+// when transitioning an entity from one system to another via component changes.
 world.addSystem(new NetworkSystem(PORT))
-world.addSystem(new LoginSystem(db))
 world.addSystem(new GameInputSystem())
+world.addSystem(new LoginSystem(db))
